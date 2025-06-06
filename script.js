19686a085d602b717624755258e49036ffe7225c
Sumit Binnani <sumit.binnani@gmail.com>
2025-06-05 17:37:20 -0700

    Stop scanner when swutching tabs

--- script.js
+++ script.js
@@ -768,6 +768,9 @@ window.onload = function() {
 
     // --- Tab Switching Logic ---
     function switchTab(tabName) {
+        // Close QR scanner when switching tabs
+        stopQrScanner(); //
+
         // Deactivate all tabs
         tabContactCardBtn.classList.remove('border-blue-600', 'text-blue-600');
         tabContactCardBtn.classList.add('text-gray-500', 'hover:text-gray-700', 'hover:border-gray-300');
